04909dc5b923b7e951ab0aa20d4be26676be6d2d
cbalsemao <cbalsemaod@gmail.com>
2024-08-04 19:51:40 +0200

    commit: wip

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,38 @@ import { useState } from "react";
 import styled from "@emotion/styled";
 import Button from "@mui/material/Button";
 import { Grid } from "@mui/material";
+import { alphabet } from "./utils/Alphabet";
 
-/*const Wrapper = styled.div`
+const Wrapper = styled.div`
   margin: 0;
   padding: 0;
   display: flex;
   flex-direction: column;
   min-height: 100vh;
   justify-content: space-between;
-`;*/
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  align-items: center;
 `;
 
-const ButtonWrapper = styled.div``;
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
-const AlphabetWrapper = styled.div``;
+const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
-const alphabet: string[] = [
-  "a",
-  "b",
-  "c",
-  "d",
-  "e",
-  "f",
-  "g",
-  "h",
-  "i",
-  "j",
-  "k",
-  "l",
-  "m",
-  "n",
-  "o",
-  "p",
-  "q",
-  "r",
-  "s",
-  "t",
-  "u",
-  "v",
-  "w",
-  "x",
-  "y",
-  "z",
-];
+const MovieWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 25%;
+`;
 
 enum GameStatus {
   ToStart = "ToStart",
@@ -58,45 +43,99 @@ enum GameStatus {
   Lose = "Lose",
 }
 
+const Movies = [
+  "Hulk",
+  "Spiderman",
+  "Ironman",
+  "Thor",
+  "Captain America",
+  "Black Widow",
+  "Doctor Strange",
+  "Antman",
+  "Black Panther",
+  "Captain Marvel",
+];
+
+const selectRandomMovie = () => {
+  const i = Math.floor(Math.random() * Movies.length);
+  return Movies[i];
+};
+
 function App() {
   const [countdown, setCountdown] = useState<number>(6);
   const [gameStatus, setGameStatus] = useState<string>(GameStatus.ToStart);
-  const [secretWord, setSecretWord] = useState<string>(
-    "Esta es una pelicula x"
-  );
+  const [secretWord, setSecretWord] = useState<string>(selectRandomMovie()); //esto hay que editarlo
+  const [word, setWord] = useState<string>("");
 
-  //ToStart - inProgres - Win - Lose
+  const secretWordLength = secretWord.length;
+  const secretWordToLowerCase = secretWord.toLowerCase();
+
+  const handleWordToGuess = (letter: string) => {
+    let newWord = "";
+
+    for (let i = 0; i < secretWordLength; i++) {
+      if (secretWordToLowerCase[i] === letter) {
+        newWord += letter;
+      } else if (secretWordToLowerCase[i] !== word[i] && word[i] !== "-") {
+        newWord += "-";
+        setCountdown(countdown - 1);
+      } else {
+        newWord += word[i];
+        setCountdown(countdown - 1);
+      }
+    }
+
+    setWord(newWord);
+  };
 
   const handleStart = () => {
     setGameStatus(GameStatus.InProgress);
   };
-  console.log("chi sta facendo?");
-  console.log(gameStatus);
 
   return (
-    <>
-      <Wrapper>
+    <Wrapper>
+      <TitleWrapper>
         <h1>Hangman Game</h1>
-        {gameStatus === GameStatus.ToStart ? (
-          <ButtonWrapper>
-            <Button onClick={handleStart} variant="contained" color="primary">
-              Start
-            </Button>
-          </ButtonWrapper>
-        ) : (
-          <>
-            <h2>Secret Word: {secretWord}</h2>
-            {alphabet.map((letter) => {
-              return (
-                <AlphabetWrapper key={letter} color="primary">
-                  <Button>{letter}</Button>
-                </AlphabetWrapper>
-              );
-            })}
-          </>
-        )}
-      </Wrapper>
-    </>
+      </TitleWrapper>
+      {gameStatus === GameStatus.ToStart ? (
+        <ButtonWrapper>
+          <Button onClick={handleStart} variant="contained" color="primary">
+            Start
+          </Button>
+        </ButtonWrapper>
+      ) : (
+        <>
+          <MovieWrapper>
+            <p>El señor ahorcado</p>
+            {word}
+          </MovieWrapper>
+
+          <Grid
+            container
+            style={{
+              backgroundColor: "lightblue",
+              width: "50%",
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "row",
+            }}
+            spacing={1}
+          >
+            {alphabet.map((letter) => (
+              <Grid item key={letter} xs={4} sm={2}>
+                <Button
+                  onClick={() => handleWordToGuess(letter)}
+                  variant="contained"
+                  color="primary"
+                >
+                  {letter}
+                </Button>
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
